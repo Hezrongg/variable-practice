0c0e93a0c935bae9d71bc65031607995c41196ce
Hezrongg <hezrongg26@gmail.com>
2024-09-13 12:34:14 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = "Hezron Gagarin";
+console.log('I am ' + name);
+
+let whatDoYouWannaBecomeinYourLife = "programmer";
+console.log('I want to become a ' + whatDoYouWannaBecomeinYourLife);
+
+let gender = "Male";
+console.log(gender);
+
+let twitterHandle = "@GagarinHezron";
+console.log(twitterHandle);
